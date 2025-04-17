959188c4f7942f031e52b711b62b7e8ac29af440
ScheffDarthusian <darthusian@gmail.com>
2025-04-17 12:24:28 +0900

    chore: wrap card component that is using  useSearchParams() in a suspense boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { PopulationDataCard } from "@/features/population/components/population-data-card";
 import PrefecturesCheckboxContainer from "@/features/prefectures/components/prefectures-checkbox-container";
+import { Suspense } from "react";
 
 export default function App() {
   return (
     <div className="flex flex-col items-center justify-center">
       <PrefecturesCheckboxContainer />
-      <PopulationDataCard />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PopulationDataCard />
+      </Suspense>
     </div>
   );
 }
