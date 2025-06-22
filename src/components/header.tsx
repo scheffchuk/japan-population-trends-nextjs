267d8187076323eecdd53ff2540d9cfd569b0015
ScheffChuk <darthusian@gmail.com>
2025-06-22 15:32:29 +0900

    chore: Add repo link in the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,20 @@
-import { TrendingUp } from "lucide-react";
+import { Github, TrendingUp } from "lucide-react";
 
 export default function Header() {
   return (
     <header className="my-10 flex w-full justify-center pt-10 text-2xl font-semibold">
-      <h1 className="flex flex-row items-center gap-2 text-slate-800 dark:text-gray-200">
+      <h1 className="relative flex flex-row items-center gap-2 text-slate-800 dark:text-gray-200">
         <TrendingUp className="h-8 w-8 text-blue-600" />
-        日本都道府県別の人口推移
+        日本の人口推移チャート
       </h1>
+      <a
+        href="https://github.com/scheffchuk/japan-population-trends-nextjs"
+        className="absolute top-10 right-25 hidden text-gray-700 md:block"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Github size={28} />
+      </a>
     </header>
   );
 }
--- src/components/ui/reset-button.tsx
+++ src/components/ui/reset-button.tsx
@@ -11,10 +11,10 @@ export default function ResetButton({ onReset, isDisabled }: Props) {
       type="button"
       onClick={onReset}
       disabled={isDisabled}
-      className="transition-full flex cursor-pointer items-center gap-1 rounded-lg px-4 py-2 text-blue-400 drop-shadow-md hover:bg-gray-50 disabled:cursor-not-allowed disabled:text-gray-400 dark:hover:bg-gray-800"
+      className="transition-full flex cursor-pointer items-center gap-1 rounded-lg border border-gray-300 px-4 py-2 text-sm text-blue-400 hover:bg-gray-200 disabled:cursor-not-allowed disabled:text-gray-400 dark:hover:bg-gray-800"
     >
-      <RotateCcw className="mr-1 h-5 w-5" />
-      リセット
+      <RotateCcw className="h-5 w-5" />
+      <span className="hidden text-sm md:block">リセット</span>
     </button>
   );
 }
